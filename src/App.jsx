a14aa33fe2feb2da24fb27fa8c0f97afbff52d3a
Kamran Ali <klashari786786@gmail.com>
2026-02-06 15:51:15 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Add from "./pages/Add.jsx"
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
 const App = () => {
   return (
     <div className='bg-gray-50 min-h-screen' >
-            
-           <>
-       
-          <Navbar  />
-         <hr />
-         <div  className='flex w-full'>
+
+      <>
+
+        <Navbar />
+        <hr />
+        <div className='flex w-full'>
           <Sidebar />
-         </div>
-         </>
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base '>
+            <Routes >
+              <Route path='/add' element={<Add />} />
+              <Route path='/list' element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+
+            </Routes>
+
           </div>
+        </div>
+      </>
+    </div>
 
   )
 }
