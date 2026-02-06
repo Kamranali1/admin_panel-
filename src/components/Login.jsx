a6a76e7923a9b00a56333af331431495a8075b76
Kamran Ali <klashari786786@gmail.com>
2026-02-06 17:22:45 +0500

    Login functionality added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { backendUrl } from '../App'
 
-const Login = () => {
+const Login = ({setToken}) => {
+    const [email,setEmail] = useState('')
+    const [password ,setPassword] = useState('')
+    
+ const onSubmitHandler = async (e)=>{
+    try{
+        e.preventDefault()
+        let response = await axios.post(backendUrl+"/api/user/admin",{email,password})
+         if(response.data.success){
+            setToken(response.data.token)
+         }else{
+            
+         }
+    }catch(error){
+        console.log(error.message)
+    }
+ }
+
   return (
-    <div>Login</div>
+    <div className='min-h-screen flex justify-center items-center'>
+        <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md' > 
+            <h1 className='text-2xl font-bold mb-4 '>Admin Panel</h1>
+            <form onSubmit={onSubmitHandler}  >
+                <div className='mb-3 min-w-72'>
+                    <p className='text-sm font-medium'>Email</p>
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} className='w-full rounded-md px-3 py-2 border border-gray-300 outline-none '  type="email" placeholder='example@forever.com' required />
+                </div >
+                <div className='mb-3 min-w-72'>
+                    <p className='text-sm font-medium' >Password</p>
+                    <input onChange={(e)=>setPassword(e.target.value)} value={password} className='w-full  rounded-md px-3 py-2 border border-gray-300 outline-none' type="Password" placeholder='********' required />
+                </div>
+           <button type='submit' className='bg-black text-white mt-2 rounded-md  w-full px-4 py-2  ' > Login</button>
+
+            </form>
+        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {assets} from "../assets/admin_assets/assets"
-const Navbar = () => {
+const Navbar = ({setToken}) => {
+   const onClickHandler = ()=>{
+     setToken('')
+  }
   return (
     <div className='flex px-[4%] py-2  justify-between'>
          <img className='w-[max(10%,80px)] ' src={assets.logo} alt="" />
-        <button className='bg-gray-500 text-white px-6 py-2 rounded-full ' >Logout </button>
+        <button onClick={onClickHandler} className='bg-gray-500 text-white px-6 py-2 rounded-full ' >Logout </button>
     </div>
   )
 }
